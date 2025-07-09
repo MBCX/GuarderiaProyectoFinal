f3f216e8a947e94e04c7ce2e69f755e31902b713
JoelClander225 <j.clander2@unapec.edu.do>
2025-07-09 17:14:21 -0400

    Add files via upload

---
+++ Guarderia.Domain/Interfaces/IAlergiaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Guarderia.Domain.Entities;
+
+namespace Guarderia.Domain.Interfaces
+{
+    public interface IAlergiaRepository
+    {
+        Task<List<Alergia>> GetAllAsync();
+        Task<Alergia?> GetByIdAsync(int id);
+        Task<List<Alergia>> GetByNiñoIdAsync(int niñoId);
+        Task AddAsync(Alergia alergia);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Guarderia.Domain/Interfaces/IIngredienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using Guarderia.Domain.Entities;
+
+namespace Guarderia.Domain.Interfaces
+{
+    public interface IIngredienteRepository
+    {
+        Task<List<Ingrediente>> GetAllAsync();
+        Task<Ingrediente?> GetByNombreAsync(string nombre);
+        Task<Ingrediente?> GetByIdAsync(int id);
+        Task AddAsync(Ingrediente ingrediente);
+        Task UpdateAsync(Ingrediente ingrediente);
+        Task DeleteAsync(int id);
+    }
+}
