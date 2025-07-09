09f67cc56de3269a88048863561b8f185db18e71
JoelClander225 <j.clander2@unapec.edu.do>
2025-07-09 17:13:47 -0400

    Add files via upload

---
+++ Guarderia.Domain/Entities/Alergia.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Guarderia.Domain.Entities
+{
+    public class Alergia
+    {
+        public int Id { get; set; } //Identificador único de la alergia
+        public Ingrediente? Ingrediente { get; set; } //Relación con el ingrediente al que el niño es alérgico 
+    }
+
+}
+
---
+++ Guarderia.Domain/Entities/Ingrediente.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Guarderia.Domain.Entities
+{
+    public class Ingrediente
+    {
+        public string? Nombre { get; set; } //Nombre del ingrediente
+    }
+
+}
