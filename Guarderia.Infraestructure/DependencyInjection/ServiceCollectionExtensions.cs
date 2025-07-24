0db5074d6f444bec5e1fc0cb29c497270409e472
MBCX <github.small792@passinbox.com>
2025-07-23 20:39:17 -0400

    Arreglado errores con el services

--- Guarderia.Infraestructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ Guarderia.Infraestructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -4,11 +4,7 @@ using Guarderia.Infraestructure.Data;
 using Guarderia.Infraestructure.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Guarderia.Infraestructure.DependencyInjection
 {
