0aa00ae6be6f0afaceb18235062c72046523cd22
MBCX <github.small792@passinbox.com>
2025-07-30 20:52:47 -0400

    Creado cambios para base de conexion de DB.

--- Guarderia.API/Program.cs
+++ Guarderia.API/Program.cs
@@ -1,3 +1,6 @@
+using Guarderia.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection") ??
+        throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.")
+    )
+);
 
 // Configure the HTTP request pipeline.
 
---
+++ Guarderia.Infraestructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guarderia.Infraestructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args = null)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("");
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
