d7bf608fa33a63f9daa6430327005f7180bd548a
MBCX <github.small792@passinbox.com>
2025-08-12 10:06:39 -0400

    Añadir Swagger para prueba de API

--- Guarderia.API/Program.cs
+++ Guarderia.API/Program.cs
@@ -1,10 +1,27 @@
 using Guarderia.Infraestructure.Data;
+using Guarderia.Infraestructure.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Infrastructure services
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+// Add CORS for development
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Development", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
@@ -15,7 +32,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors("Development");
+}
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
